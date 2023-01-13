efecd4fcce2815e3498d8fc2d2ae7c551ae1c617
zqsd <zqsd@sfr.fr>
2023-01-13 21:07:45 +0100

    add new client page

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -14,21 +14,92 @@ class ClientController extends AbstractController
 
     public function homepage(): Response
     {
-  
-        $clients = ['client1','client2'];
-        return $this->render('client/homepage.html.twig',[
-            'title'=>'coucou',
-            'clients'=>$clients,
+        
+        $clients = [
+            [
+                "id" => "1",
+                "nom" => "a",
+                "prenom" => "b",
+                "phone" => "0123456789",
+            ],
+            [
+                "id" => "2",
+                "nom" => "t",
+                "prenom" => "t",
+                "phone" => "0123456789",
+            ],
+            [
+                "id" => "3",
+                "nom" => "o",
+                "prenom" => "m",
+                "phone" => "0123456789",
+            ],
+            [
+                "id" => "4",
+                "nom" => "p",
+                "prenom" => "b",
+                "phone" => "0123456789",
+            ],
+        ];
+        return $this->render('client/homepage.html.twig', [
+            'title' => 'coucou',
+            'clients' => $clients,
         ]);
     }
 
     /**
-     * @Route("/client/{clientid}", name="nouveauClient")
+     * @Route("/client/{clientid}", name="detailClient")
+ 
      */
-    public function nouveauClient($clientid = null): Response
+    public function detailClient($clientid = null): Response
     {
-        
+
         return new Response("browse:$clientid");
         // return $this->render('$0.html.twig', []);
+    }
+
+    /**
+     * @Route("/newclient", name="newclient")
+     */
+    public function newClient(): Response
+    {
+        return $this->render('/client/nouveauClient.html.twig', []);
+    }
+
+
+    /**
+     * @Route("/api/clients", name="api_clients")
+     * @method({"GET","HEAD"})
+     */
+    public function getClients(): Response
+    {
+        $clients = [
+            [
+                "id" => "1",
+                "nom" => "a",
+                "prenom" => "b",
+                "phone" => "0123456789",
+            ],
+            [
+                "id" => "2",
+                "nom" => "t",
+                "prenom" => "t",
+                "phone" => "0123456789",
+            ],
+            [
+                "id" => "3",
+                "nom" => "o",
+                "prenom" => "m",
+                "phone" => "0123456789",
+            ],
+            [
+                "id" => "4",
+                "nom" => "p",
+                "prenom" => "b",
+                "phone" => "0123456789",
+            ],
+        ];
+        
+        return $this->json($clients);
     }
 }
